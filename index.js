9a5e51049ac1ef83834b53f20bc802e37650f776
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-11-24 01:27:19 +0600

    server side connect to database

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zr5yxev.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productsCollection = client.db("usedLaptop").collection("laptop");
+    console.log("database Connected");
+  } finally {
+  }
+}
+run().catch((error) => {
+  console.log(error);
+});
+
 app.get("/", (req, res) => {
   res.send("server running");
 });
