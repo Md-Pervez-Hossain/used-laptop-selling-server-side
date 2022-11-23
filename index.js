094468b9af06b72e2e473b20009c91324c88f601
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-11-23 21:57:26 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+app.listen(port, () => {
+  console.log("server running");
+});
