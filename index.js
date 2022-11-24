fae38058aff90907a26d301ccbf02dd26d4c4fbf
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-11-24 19:52:33 +0600

    booking api make and updateproduct status

--- index.js
+++ index.js
@@ -21,10 +21,27 @@ async function run() {
     const buyerBookingCollection = client
       .db("usedLaptop")
       .collection("buyerBooking");
+    const usersCollection = client.db("usedLaptop").collection("users");
     //add products api
     app.post("/addproducts", async (req, res) => {
       const products = req.body;
       const result = await productsCollection.insertOne(products);
+      res.send(result);
+    });
+    app.put("/addproducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = {
+        $set: {
+          booked: true,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updatedProduct,
+        options
+      );
       res.send(result);
     });
 
@@ -45,7 +62,13 @@ async function run() {
       const result = await productsCollection.findOne(query);
       res.send(result);
     });
-
+    //users api
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
+    //booking api
     app.post("/buyerBooking", async (req, res) => {
       const buyerBooking = req.body;
       const booking = await buyerBookingCollection.insertOne(buyerBooking);
